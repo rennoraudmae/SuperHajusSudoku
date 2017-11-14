e4185c7c13305c534e212cce208d28c797d97f4f
Toomas Kaasik <ktoomas2@gmail.com>
2017-11-14 22:59:52 +0200

    cell focus working

--- client/game_field.py
+++ client/game_field.py
@@ -38,16 +38,16 @@ class GameField(Frame):
         if self.canvas.find_withtag(CURRENT):
             tags = [tag for tag in self.canvas.gettags(CURRENT)]
             if 'cell' in tags:
+                if self.focused_cell != None:
+                    self.canvas.itemconfig('cell', width=1, outline='black')
+                    self.canvas.tag_lower('cell')
+                    self.canvas.update_idletasks()
+                self.focused_cell = (int(tags[0][0]), int(tags[0][1]))
                 self.canvas.itemconfig(CURRENT, width=3, outline='red')
                 self.canvas.tag_raise(CURRENT)
                 self.canvas.tag_raise('numbers')
                 self.canvas.update_idletasks()
-                if self.focused_cell != None:
-                    if self.canvas.find_withtag('%d%d'%self.focused_cell): print 'yes'
-                    self.canvas.itemconfig('%d%d'%self.focused_cell, width=1, outline='black')
-                    self.canvas.tag_lower('%d%d'%self.focused_cell)
-                    self.canvas.update_idletasks()
-                self.focused_cell = (int(tags[0][0]), int(tags[0][1]))
+
 
     def draw_field(self):
         #draws sudoku field on the canvas
@@ -56,11 +56,12 @@ class GameField(Frame):
         
         for i in range(9):
             for j in range(9):
-                x0 = C.PADDING + i * C.CELL_SIDE
-                y0 = C.PADDING + j * C.CELL_SIDE
+                x0 = C.PADDING + j * C.CELL_SIDE
+                y0 = C.PADDING + i * C.CELL_SIDE
                 x1 = x0 + C.CELL_SIDE
                 y1 = y0 + C.CELL_SIDE
-                self.canvas.create_rectangle(x0, y0, x1, y1, tags=('%d%d'%(j,i), 'cell'), fill='white')
+                self.canvas.create_rectangle(x0, y0, x1, y1, tags=('%d%d'%(i,j), 'cell'), fill='white')
+                #self.canvas.create_text(x0 + C.CELL_SIDE/2, y0 + C.CELL_SIDE/2, text='%d%d'%(i,j))
         for i in range(3):
             for j in range(3):
                 x0 = C.PADDING + i*3 * C.CELL_SIDE
